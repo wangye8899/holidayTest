f8d2d4cfb46efe7f3e718c1bf25f7f65d52a9e19
wangye8899 <3091485316@qq.com>
2018-01-18 09:27:43 +0800

    git commit -m "vscode"

---
+++ java算法/Stack.java
@@ -0,0 +1,64 @@
+/*
+ * @Author: HH.wangye 
+ * @Date: 2018-01-18 09:23:43 
+ * @Last Modified by:   HH.wangye 
+ * @Last Modified time: 2018-01-18 09:23:43 
+ */
+
+
+
+public class Stack
+{
+    private int maxsize;
+    private int top=-1;
+    int [] arraystack;
+    public Stack(int length)
+    {
+        maxsize = length;
+        arraystack = new int [maxsize];
+    }
+//进栈
+    public void push(int value)
+    {
+        //栈顶指针先上移，然后赋值
+        arraystack[++top] = value;
+    }
+    public void pop()
+    {
+        int value = arraystack[top--];
+        System.out.print(value); 
+    } 
+    public boolean empty()
+    {
+        if(top==-1)
+        {
+            System.out.print("您的栈为空");
+            return true; 
+        }
+        else
+        {
+            return false; 
+        }
+    }
+    public void full()
+    {
+        if(top == maxsize-1)
+        {
+            System.out.print("栈已经满");
+        }
+    }
+    public static void main(String [] argv)
+    {
+        Stack stack = new Stack(10);
+        stack.push(0);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        while(!stack.empty())
+        {
+            stack.pop();
+        }
+    }
+
+
+}
